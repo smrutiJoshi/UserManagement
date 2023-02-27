c37d3344cdda5350df643d2b0102dd747d926dc0
smrutiJoshi <smruti@tentwenty.me>
2023-02-27 17:21:49 +0530

    Changed to Promises

--- service/user/user.service.js
+++ service/user/user.service.js
@@ -1,54 +1,62 @@
 const pool = require("../../config/database");
 
 module.exports = {
-    create: (data, callback) => {
-        console.log(data.contact);
-        pool.query(
-            `insert into registration(firstName,lastName,gender,email,password,contact)
-            values(?,?,?,?,?,?)`,
-            [
-                data.first_name,
-                data.last_name,
-                data.gender,
-                data.email,
-                data.password,
-                data.contact
-            ],
-            (error, results, fields) => {
-                if (error) {
-                    return callback(error)
+    create: async(data) => {
+        return new Promise((resolve,reject) => {
+            pool.query(
+                `insert into registration(firstName,lastName,gender,email,password,contact)
+                values(?,?,?,?,?,?)`,
+                [
+                    data.first_name,
+                    data.last_name,
+                    data.gender,
+                    data.email,
+                    data.password,
+                    data.contact
+                ],
+                (error, results, fields) => {
+                    if (error) {
+                        return reject(error)
+                    }
+                    return resolve(results.affectedRows)
                 }
-                return callback(null, results)
-            }
-        )
+            )
+        });
+      
     },
-    getUsers: callback => {
-        pool.query(
-            `select id, firstName,lastName,gender,email,password,contact from registration`,
-            [],
-            (error, results, fields) => {
-                if (error) {
-                    return callback(error)
+    getUsers: async() => {
+        return new Promise((resolve,reject)=>{
+            pool.query(
+                `select id, firstName,lastName,gender,email,password,contact from registration`,
+                [],
+                (error, results, fields) => {
+                    if (error) {
+                        return reject(error)
+                    }
+                    return resolve(results)
                 }
-                return callback(null, results)
-            }
-        );
+            );
+        })
+        
     },
 
-    getUserByUserId: (id,callback) => {
-        pool.query(
-            `select id, firstName,lastName,gender,email,password,contact from registration where id = ?`,
-            [id],
-            (error, results, fields) => {
-                if (error) {
-                    return callback(error)
+    getUserByUserId: async(id) => {
+        return new Promise((resolve,reject) => {
+            pool.query(
+                `select id, firstName,lastName,gender,email,password,contact from registration where id = ?`,
+                [id],
+                (error, results, fields) => {
+                    if (error) {
+                        return reject(error)
+                    }
+                    return resolve(results[0])
                 }
-                return callback(null, results[0])
-            }
-        );
+            );
+        })
+        
     },
-    updateUser: (data, callback) => {
-        console.log(data.id);
+    updateUser: async (data) => {
+      return new Promise((resolve, reject)=> {
         pool.query(
             `update registration set firstName=?,lastName=?,gender=?,email=?,password=?,contact=? where id = ?`,
             [
@@ -62,53 +70,64 @@ module.exports = {
             ],
             (error, results, fields) => {
                 if (error) {
-                    return callback(error)
+                    return reject(error)
                 }
-                return callback(null, results.affectedRows)
+                return resolve(results.affectedRows)
             }
         );
+      })
+        
     },
 
-    deleteUser: (data, callback) => {
-        pool.query(
-            `delete from registration where id = ?`,
-            [data.id],
-            (error, results, fields) => {
-                if (error) {
-                    return callback(error)
+    deleteUser: async(data) => {
+        return new Promise((resolve, reject)=> {
+            pool.query(
+                `delete from registration where id = ?`,
+                [data.id],
+                (error, results, fields) => {
+                    if (error) {
+                        return reject(error)
+                    }
+                    console.log(results);
+                    return resolve(results.affectedRows)
                 }
-                return callback(null, results[0])
-            }
-        )
+            )
+        });
+        
     },
 
-    getUserByUserEmail: (email, callback) => {
-        pool.query(
-          `select * from registration where email = ?`,
-          [email],
-          (error, results, fields) => {
-            if (error) {
-                callback(error);
-            }
-            return callback(null, results[0]);
-          }
-        );
+    getUserByUserEmail: async(email) => {
+        console.log(email);
+        return new Promise((resolve,reject) => {
+            pool.query(
+                `select * from registration where email = ?`,
+                [email],
+                (error, results, fields) => {
+                  if (error) {
+                      return reject(error);
+                  }
+                  return resolve(results[0]);
+                }
+              );
+        }); 
       },
 
-      updateProfile : (data, callback) => {
-        pool.query(
-            `update registration set profile= ? where id=?`,
-            [
-                data.file,
-                data.id
-            ],
-            (error, results, fields) => {
-                if (error) {
-                    return callback(error)
+      updateProfile : async(data) => {
+        return new Promise((resolve,reject)=>{
+            pool.query(
+                `update registration set profile= ? where id=?`,
+                [
+                    data.file,
+                    data.id
+                ],
+                (error, results, fields) => {
+                    if (error) {
+                        return reject(error)
+                    }
+                    return resolve(results.affectedRows)
                 }
-                return callback(null, results[0])
-            }
-
-        )
+    
+            )
+        });
       }
 };
